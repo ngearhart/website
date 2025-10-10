e1daaca6d55cbbea76a77917535cbf904600db1b
Noah Gearhart <excelsiorvfx@gmail.com>
2025-10-10 09:50:25 -0400

    Initial commit for restyle

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Noah Gearhart';
+export const SITE_DESCRIPTION = 'Noah is a Cloud Platform Engineer and Security Enthusiast. Visit this website to learn more about him, his past work, his open-source contributions, his blog, and more.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
